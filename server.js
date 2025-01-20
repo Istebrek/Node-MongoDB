4e40cd0f0783171b9388973f4e62a17b0f1ef7a4
Istebrek <istebrek.akram@outlook.com>
2025-01-20 15:07:41 +0100

    added more endpoints

--- server.js
+++ server.js
@@ -20,6 +20,43 @@ app.post ("/blogpost", (req,res) => {
     res.send("This is a post request");
 });
 
+app.put("/api/product/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findbyIdAndUpdate(id, req.body);
+        if (!product) {
+            return res.status(400).json({message:"Product does not exist"});
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500);
+    }
+})
+
+
+app.get("/api/product/:id", async (req, res) => { //detta är read metoden för ett id. istället för :id i endpointen kan man lägga in id:et för produkten från databasen i webbaddressen. så: http://localhost:3000/api/product/678e50eaa600744a2572df91
+    try {
+        const {id} = req.params;
+        const product = await Product.findByID(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500);
+    }
+})
+
+app.get("/api/products", async (req, res) => { //detta är som read metoden. för att visa alla produkter. detta är read all. ska även ha en readby id från databasen.
+    try {
+        const products = await Product.find({}); //efter find är måsvingarna tomma vilket säger åt servern att hämta och visa allt
+        res.status(200).json(products);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+});
+
+
+
 app.post("/api/product", async (req, res) => { //som en create metod
     try {
         const product = await Product.create(req.body);
@@ -29,11 +66,11 @@ app.post("/api/product", async (req, res) => { //som en create metod
         res.status(404);
     }
 
-
-
     // console.log(req.body);
     // res.send(req.body);
 });
+
+
 
 
 
