5dee5e76caddf7b17a460659957a49697674172c
Istebrek <istebrek.akram@outlook.com>
2025-01-20 11:47:33 +0100

    added connection to mongo

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
 const express = require ("express");
 const app = express();
+const mongoose = require ("mongoose");
+
 const PORT = 3000;
 
+app.get("/", (req,res) => {
+    res.send("Welcome to my MongoAPI");
+});
 
-app.listen(PORT, () =>{
-    console.log("Listening to " + PORT);
-})
+app.post ("/blogpost", (req,res) => {
+    res.send("This is a post request");
+});
+
+mongoose.connect("mongodb+srv://Istebrek:123456admin@istesapi.yoxj0.mongodb.net/Products-API?retryWrites=true&w=majority&appName=IstesAPI")
+.then(() => {
+    console.log("Connected to Mongo");
+    
+    app.listen(PORT, () =>{
+        console.log("Listening to " + PORT);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
